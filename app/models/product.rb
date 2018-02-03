b5122a4080508ccf4a7333ad2a2a120da2b57322
Igor Sant'ana <igorba2005@gmail.com>
2018-02-03 11:36:32 -0300

    Validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :clients, through: :orders
+
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
+
 end

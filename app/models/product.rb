ef45c79c93a2748c7efd0615089edfc31d945e67
Igor Sant'ana <igorba2005@gmail.com>
2018-02-02 23:06:34 -0300

    Finishing crud

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  has_many :orders, dependent: :destroy
+  has_many :clients, through: :orders
 end
--- db/migrate/20180202213007_create_orders.rb
+++ db/migrate/20180202213007_create_orders.rb
@@ -8,8 +8,8 @@ class CreateOrders < ActiveRecord::Migration[5.1]
 =end
 
     create_table :orders do |t|
-      t.references  :product, foreign_key: true, null: false
-      t.references  :client, foreign_key: true, null: false
+      t.references  :product, foreign_key: true, null: false, on_delete: :cascade
+      t.references  :client, foreign_key: true, null: false, on_delete: :cascade
     end
 
   end
